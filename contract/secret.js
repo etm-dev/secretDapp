b57b079dfb8d0e207e27cdee3fb03f25a802c9a7
ray <364664251@qq.com>
2019-03-15 14:06:49 +0800

    添加存储信息接口

--- contract/secret.js
+++ contract/secret.js
@@ -23,29 +23,40 @@ module.exports = {
       return INVALIDATE_NICKNAME
     }
     await app.model.User.create({
-      address:sender,
+      address: sender,
       publicKey,
       image,
       nickname
     })
 
   }
-  // save: async function(words, receiveAddress) {
-    /*let sender = this.trs.senderId
+  save: async function(words, receiveAddress) {
+    let sender = this.trs.senderId
     if (!etmjs.crypto.isAddress(sender)) return INVALIDATE_USER
     if (!etmjs.crypto.isAddress(receiveAddress)) return INVALIDATE_RECEIVER
     //将接收者地址转换成publicKey
     let publicKey = null
     //1.在用户表中查询是否有该用户，有该用户就读取publickey
-    //2.在主网中获取是否有publickey （如果是没注册的账户  是没有publickey存在的）
-    try {
-      let res = await axios.get("http://etm.red:8097/api/accounts/getPublickey?address=" + receiveAddress, {
-        timeout: 5000
-      })
-      if(data.success){
-        publicKey=data.publicKey
+    let receiveUser = await app.model.User.findOne({
+      condition: {
+        address: receiveAddress
       }
-    } catch (error) {
+    })
+    if (receiveUser) {
+      publicKey=receiveUser.publicKey
+    }
+    //2.在主网中获取是否有publickey （如果是没注册的账户  是没有publickey存在的）
+    if (!receiveUser) {
+      try {
+        let res = await axios.get("http://etm.red:8097/api/accounts/getPublickey?address=" + receiveAddress, {
+          timeout: 5000
+        })
+        if (data.success) {
+          publicKey = data.publicKey
+        }
+      } catch (error) {
+
+      }
     }
     if (!publicKey) {
       return INVALIDATE_PUBLIC_KEY
@@ -64,15 +75,14 @@ module.exports = {
       return ENCODE_STRING_ERROR
     }
 
-    //单步添加单词
     app.sdb.create('Word', {
       'id': ids.generateID(),
       'msg': encodeMsg,
       'sender': sender,
-
-
+      'receiver':receiveAddress,
+      date:Date.now()
     })
-    */
-  // }
+
+  }
 
 }
