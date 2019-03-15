e0b591342c361acadf46742e484d2127b5495c6d
ray <364664251@qq.com>
2019-03-15 15:51:28 +0800

    提交所有接口

--- interface/secret.js
+++ interface/secret.js
@@ -67,7 +67,8 @@ app.route.get("/msg/:address", async req => {
   }
 })
 
-
+//http://etm.red:8097/api/dapps/27c1e5b0bd7f659298ca7ce8223d9958a2b0b696e5b0fd5001941da21d1233c6/decode
+//demo: http://etm.red:8097/api/dapps/27c1e5b0bd7f659298ca7ce8223d9958a2b0b696e5b0fd5001941da21d1233c6/decode?encodeMsg=0e141fa52463531fb654c2398cabf883ef194130af99a88e9e34669cb995e40c5de9cbe03a081a196dff375a719dc5b930435ebe9a36f31f821a1b22&secret=foot%20profit%20decorate%20orient%20quit%20goose%20upon%20curve%20coast%20warm%20income%20manual
 //所有的解密操作都可以在本地实现，本示例仅仅为了更好的给大家演示
 //TODO  如果开发者觉得这样不安全，完全可以把加密解密放到本地操作，此案例只是更好的给大家演示
 app.route.get("/decode", async req => {
@@ -75,12 +76,16 @@ app.route.get("/decode", async req => {
     encodeMsg,
     secret
   } = req.query
+  //由于secret是带空格的，所有需要在发送请求的时候进行encodeURI
+  //此处就是decodeURI
+  encodeMsg = decodeURI(encodeMsg)
+  secret=decodeURI(secret)
   //既然已经有了secret 那么说明不需要再验证地址之类的合法性，secret代表最大权限
   if (!secret) {
     return DECODE_STRING_ERROR
   }
   try {
-    dmsg = cryptoUtils.decodeString(encodedMsg, secret)
+    dmsg = cryptoUtils.decodeString(encodeMsg, secret)
   } catch (e) {
     return DECODE_STRING_ERROR
   }
